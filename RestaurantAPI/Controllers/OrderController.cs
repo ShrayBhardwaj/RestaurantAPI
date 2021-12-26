509a2840ad07ad6b7327e8f3dd490206f0ccca31
shray <Shray2037@gmail.com>
2021-12-26 15:24:06 +0530

    API Added

---
+++ RestaurantAPI/Controllers/OrderController.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using RestaurantAPI.Models;
+
+namespace RestaurantAPI.Controllers
+{
+    public class OrderController : ApiController
+    {
+        private DBModel db = new DBModel();
+
+        // GET: api/Order
+        public System.Object GetOrders()
+        {
+            var result = (from a in db.Orders
+                          join b in db.Customers on a.CustomerId equals b.CustomerID
+
+                          select new
+                          {
+                              a.OrderId,
+                              a.OrderNo,
+                              Customer = b.Name,
+                              a.PMethod,
+                              a.Gtotal
+                          }).ToList();
+
+            return result;
+        }
+
+        // GET: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult GetOrder(long id)
+        {
+            var order = (from a in db.Orders
+                         where a.OrderId == id
+
+                         select new
+                         {
+                             a.OrderId,
+                             a.OrderNo,
+                             a.CustomerId,
+                             a.PMethod,
+                             a.Gtotal,
+                             DeletedOrderItemIDs = ""
+                         }).FirstOrDefault();
+
+            var orderDetails = (from a in db.OrderItems
+                                join b in db.Items on a.ItemID equals b.ItemID
+                                where a.OrderId == id
+
+                                select new
+                                {
+                                    a.OrderId,
+                                    a.OrderItemID,
+                                    a.ItemID,
+                                    ItemName = b.Name,
+                                    b.Price,
+                                    a.Quantity,
+                                    Total = a.Quantity * b.Price
+                                }).ToList();
+
+            return Ok(new { order, orderDetails });
+        }
+
+        // POST: api/Order
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult PostOrder(Order order)
+        {
+            try
+            {
+                //Order table
+                if (order.OrderId == 0)
+                    db.Orders.Add(order);
+                else
+                    db.Entry(order).State = EntityState.Modified;
+
+                //OrderItems table
+                foreach (var item in order.OrderItems)
+                {
+                    if (item.OrderItemID == 0)
+                        db.OrderItems.Add(item);
+                    else
+                        db.Entry(item).State = EntityState.Modified;
+                }
+
+                //Delete for OrderItems
+                foreach (var id in order.DeletedOrderItemIDs.Split(',').Where(x => x != ""))
+                {
+                    OrderItem x = db.OrderItems.Find(Convert.ToInt64(id));
+                    db.OrderItems.Remove(x);
+                }
+
+
+                db.SaveChanges();
+
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+
+        // DELETE: api/Order/5
+        [ResponseType(typeof(Order))]
+        public IHttpActionResult DeleteOrder(long id)
+        {
+            Order order = db.Orders.Include(y => y.OrderItems)
+                .SingleOrDefault(x => x.OrderId == id);
+
+            foreach (var item in order.OrderItems.ToList())
+            {
+                db.OrderItems.Remove(item);
+            }
+
+            db.Orders.Remove(order);
+            db.SaveChanges();
+
+            return Ok(order);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool OrderExists(long id)
+        {
+            return db.Orders.Count(e => e.OrderId == id) > 0;
+        }
+    }
+}
